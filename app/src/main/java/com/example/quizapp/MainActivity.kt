9518b89ca147acf81f83bc6517aa7710ef4f01ca
Theo-Ba <snibot123@gmail.com>
2022-09-30 09:15:04 -0700

    loaded questions into mainActivity, and added layout for buttons and text

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+        // step 1: open the raw resource as an InputStream
+        // R.raw.questions --> R is the Resources class, raw is folder,
+        // questions is the file
+        val inputStream = resources.openRawResource(R.raw.questions)
+        // step 2: use a buffered reader on the inputstream to read the
+        // the text into a string. we call it jsonString because it's the entire JSON file in a string
+        val jsonString = inputStream.bufferedReader().use {
+            // the last line of the use function is returned
+            it.readText()
+        }
+        Log.d(TAG, "onCreate: $jsonString")
+
+        // use gson to convert the jsonText into a List<Question>
+        // https://medium.com/@hissain.khan/parsing-with-google-gson-library-in-android-kotlin-7920e26f5520
+        // check the section called "Parsing between a Collection, List, or Array"
+        // log the list of questions and see if your Question objects all appear correct
+        val gson = Gson()
+        // gson needs to know what kind of list you are converting to
+        // typetoken tells gson it will be a List<Question>
+
+        val type = object : TypeToken<List<Question>>() { }.type
+        val questions = gson.fromJson<List<Question>>(jsonString, type)
+    }
+
+    private fun wireWidgets() {
+
     }
 }
